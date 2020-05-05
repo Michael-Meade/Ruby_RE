e1383a8a977e12ff0eb1c0414ac1c626b57daf4a
Michael Meade <47438130+Michael-Meade@users.noreply.github.com>
2020-05-05 14:11:52 -0400

    Delete test.rb

--- test.rb
+++
@@ -1,7 +0,0 @@
-require_relative 'lib/re'
-#RE::Gzip.ungzip("5504f0d7fc3d0add9303d7a9012ef40775138987a25578611b4a5ae3cd73d8be.bin.gz")
-
-#RE::Commands.file_command(file_name)
-
-#RE::Crypto.crypto_test("ldm")
-RE::Commands.detect_crontab("cryptocurrency_mining_malware_-_elasticsearch___weblogic.sh")
