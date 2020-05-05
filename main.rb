cb0a2b88fb8af62dc1ffc4c37fe0c53440c163cd
Michael-Meade <mameade@utica.edu>
2020-05-05 13:49:56 -0400

    changed arg

--- main.rb
+++ main.rb
@@ -1,35 +1,23 @@
 require 'open3'
 require 'colorize'
-#require_relative 'lib/commands'
-#require_relative 'lib/utils'
 require_relative 'lib/re'
 require_relative 'dl/the_downloader'
 
 
-print"
 
-████████╗██╗  ██╗███████╗    ██╗      ██████╗  ██████╗ ██╗  ██╗███████╗██████╗
-╚══██╔══╝██║  ██║██╔════╝    ██║     ██╔═══██╗██╔═══██╗██║ ██╔╝██╔════╝██╔══██╗
-   ██║   ███████║█████╗      ██║     ██║   ██║██║   ██║█████╔╝ █████╗  ██████╔╝
-   ██║   ██╔══██║██╔══╝      ██║     ██║   ██║██║   ██║██╔═██╗ ██╔══╝  ██╔══██╗
-   ██║   ██║  ██║███████╗    ███████╗╚██████╔╝╚██████╔╝██║  ██╗███████╗██║  ██║
-   ╚═╝   ╚═╝  ╚═╝╚══════╝    ╚══════╝ ╚═════╝  ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝
-
-".red
-print("Looker>".green)
-opt = gets.chomp.split
-case opt[0]
-when "run"
-    if opt[1].nil?
-    	puts "use file"
-    end
-    RE::Commands.new_file(opt[1])
-    RE::Commands.file_command(opt[1])
-    RE::Commands.strings_command(opt[1])
-    RE::Commands.detect_urls(opt[1])
-    RE::Commands.detect_strings(opt[1])
-    RE::Commands.hash_file(opt[1])
-    RE::Crypto.all(opt[1])
-    The_downloader.download_files(opt[1])
-    #Crypto.detect_base64(opt[1])
+if ARGV[0].nil?
+  puts "EX: ruby main.rb file"
+else
+  if !File.exist?(ARGV[0])
+      puts "File Doesnt exist"
+  elsif ARGV[1].nil?
+    RE::Commands.new_file(ARGV[0])
+    RE::Commands.file_command(ARGV[0])
+    RE::Commands.strings_command(ARGV[0])
+    RE::Commands.detect_urls(ARGV[0])
+    RE::Commands.detect_strings(ARGV[0])
+    RE::Commands.hash_file(ARGV[0])
+    RE::Crypto.all(ARGV[0])
+    The_downloader.download_files(ARGV[0])
+  end
 end
